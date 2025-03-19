38ad71324508faabb5f8bc1f8b241268ecd008d6
Omar Garcia <omar.garcia@morsa.com.mx>
2025-03-19 10:06:12 -0600

    Leyendo el valor seleccionado del PICKER

--- cotizadorCriptoMonedas/components/Formulario.js
+++ cotizadorCriptoMonedas/components/Formulario.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { View, Text, StyleSheet } from'react-native';
 
 import { Picker } from '@react-native-picker/picker';
 
 const Formulario = () => {
+    const [moneda, guardarMoneda] = useState('');
+    const [criptomoneda, guardarCriptoMoneda] = useState('');
+
+    const obtenerMoneda = moneda => {
+        console.log('obtener moneda', moneda);
+
+        guardarMoneda(moneda);
+    };
+
     return (
         <View>
             <Text style={styles.label}>Moneda</Text>
 
-            <Picker>
+            <Picker
+            selectedValue={moneda}
+                onValueChange={ moneda => obtenerMoneda(moneda) }
+            >
                 <Picker.Item label="- Seleccione -" value="USD" />
-                <Picker.Item label="- EUA -" value="USD" />
-                <Picker.Item label="- MX -" value="MXN" />
-                <Picker.Item label="- EURO -" value="" />
-                <Picker.Item label="- LIBRA ESTERLINA -" value="GBP" />
+                <Picker.Item label="Dolar de Estados Unidos"  value="USD" />
+                <Picker.Item label="Peso Mexicano"  value="MXN" />
+                <Picker.Item label="Euro"  value="" />
+                <Picker.Item label="Libra Esterlina"  value="GBP" />
             </Picker>
 
             <Text style={styles.label}>Criptomoneda</Text>
