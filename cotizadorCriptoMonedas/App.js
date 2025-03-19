a57d65b23e2c7706ffd371aa846416796f1a5533
Omar Garcia <omar.garcia@morsa.com.mx>
2025-03-19 00:18:29 -0600

    Reiniciar el índice y agregar todos los archivos

---
+++ cotizadorCriptoMonedas/App.js
@@ -0,0 +1,29 @@
+
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView >
+
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
