479a5c5c47c66792efb236f9afa1c562815e3b53
Omar Garcia <omar.garcia@morsa.com.mx>
2025-03-19 09:47:34 -0600

    Creando FORMULARIO

--- cotizadorCriptoMonedas/App.js
+++ cotizadorCriptoMonedas/App.js
@@ -12,7 +12,9 @@ import {
   View,
   Image
 } from 'react-native';
+
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 const App = () => {
 
@@ -24,6 +26,10 @@ const App = () => {
         style={styles.imagen}
         source= { require('./assets/img/cryptomonedas.png') }
       />
+
+      <View style={styles.contenido}>
+        <Formulario />
+      </View>
     </>
   );
 };
@@ -32,6 +38,10 @@ const styles = StyleSheet.create({
   imagen: {
     width: '100%',
     height: 150,
+    marginHorizontal: '2.5%'
+  },
+
+  contenido: {
     marginHorizontal: '2.5%'
   },
 });
