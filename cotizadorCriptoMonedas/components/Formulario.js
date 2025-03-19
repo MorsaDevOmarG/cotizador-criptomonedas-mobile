c033c4273a1aa92f031eb00f42f192d5057e9b5a
Omar Garcia <omar.garcia@morsa.com.mx>
2025-03-19 09:58:23 -0600

    Agregando PICKER

--- cotizadorCriptoMonedas/components/Formulario.js
+++ cotizadorCriptoMonedas/components/Formulario.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 import { View, Text, StyleSheet } from'react-native';
 
+import { Picker } from '@react-native-picker/picker';
+
 const Formulario = () => {
     return (
         <View>
             <Text style={styles.label}>Moneda</Text>
+
+            <Picker>
+                <Picker.Item label="- Seleccione -" value="USD" />
+                <Picker.Item label="- EUA -" value="USD" />
+                <Picker.Item label="- MX -" value="MXN" />
+                <Picker.Item label="- EURO -" value="" />
+                <Picker.Item label="- LIBRA ESTERLINA -" value="GBP" />
+            </Picker>
 
             <Text style={styles.label}>Criptomoneda</Text>
         </View>
