0310fd020ea8f2cab56008060219ba576607f51b
Omar <omar.garcia@morsa.com.mx>
2026-02-20 00:50:32 -0600

    Iniciando proyecto

---
+++ App.js
@@ -0,0 +1,20 @@
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>Hola mundo</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Proyecto: Cotizador Cripto Monedas
 
+- Creación proyecto:
+  - **_npx @react-native-community/cli init nombreProyecto_**
+
 ## Herramientas y/o Tecnologías
 
 - React
